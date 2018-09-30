92b18b2f0259082d2ed452509771b971edb3d981
Jacob Nouwatin <jacobnouwatin@Jacobs-MacBook-Pro.local>
2018-09-30 07:57:25 +0100

    chore(Deployment): Application deployment
    - Setup heroku pipeline
    - add app.json file for review app deployment
    
    [Finishes #160794868]

--- client/src/config/host.js
+++ client/src/config/host.js
@@ -1,4 +1,4 @@
 const host = window.location.hostname === 'localhost' ?
-  'http://localhost:8000/v1' : 'https://andela-teams-core.herokuapp.com/v1/';
+  'http://localhost:8000/v1' : 'https://ghoulies-taps-server.herokuapp.com/v1/';
 
 export default host;
