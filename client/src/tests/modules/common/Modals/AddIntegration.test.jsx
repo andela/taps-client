51c355ae491215173f50beb2812ef6880687f607
Samuel Afolaranmi <sammysgame.dev@gmail.com>
2018-10-12 11:33:23 +0100

    ch(tests): common module tests - write tests for Footer component - write tests for Navbar component - write tests for Modal components - write tests for Spinner component - write tests for Preloader component [Starts #161117046]

---
+++ client/src/tests/modules/common/Modals/AddIntegration.test.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+import AddIntegration from '../../../../modules/common/Modals/AddIntegration';
+
+describe('<AddIntegration />', () => {
+  it('should mount without crashing', () => {
+    const wrapper = shallow(<AddIntegration />);
+    expect(wrapper.find('p').get(0).props.children).toBe('Select an integration to add');
+    expect(wrapper.find('p').get(1).props.children).toBe('Github Repository');
+    expect(wrapper.find('p').get(2).props.children).toBe('Pivotal Tracker');
+    expect(wrapper.find('p').get(3).props.children).toBe('Slack Channel (private)');
+    expect(wrapper.find('p').get(4).props.children).toBe('Slack Channel (public)');
+  });
+});
