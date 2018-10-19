ad3cea76cbeb0fda5e9d4161a606f4808186643c
Veeqtor <nwokeochavictor@gmail.com>
2018-10-19 13:30:02 +0100

    bg(error-page): Fix the 404 error page write-up
    - Re-write the error page write up
    
    [Delivers #161342131]

--- client/src/modules/common/404/index.jsx
+++ client/src/modules/common/404/index.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import Emoji from 'react-emoji-render';
 import img from '../../../../public/resources/images/404-Logo.png';
 
 
@@ -7,14 +8,17 @@ const ErrorPage = () => (
     <div className="row">
       <div className="col s12 center-align m-15-auto" >
         <img src={img} alt="404 error" className="responsive-img animate" width="400" height="150" />
-        <p className="flow-text valign-wrapper" >
-        Unfortunately the page you are looking for could not be found. It may have been removed,
-        had its name changed or is temporarily unavailable.
+        <p className="flow-text center-align" style={{ fontSize: "2.2rem" }}>
+          {"Oops!! you are lost and that\'s not "}<strong>TIA</strong>
+          <Emoji text=":/" />
         </p>
-        <p className="flow-text" >
-        Check the URL you entered for mistakes and try again.
+        <p className="flow-text center-align" >
+          <small style={{ fontSize: "1.1rem" }}>
+            Just kidding, seems like something broke,
+            or we could not find what you were looking for.
+          </small>
         </p>
-        <a href="/teams" className="top-margin bottom-margin waves-effect waves-light btn blue-btn">Go back to Home page</a>
+        <a href="/teams" className="top-margin bottom-margin waves-effect waves-light btn blue-btn">Lets take you back</a>
       </div>
     </div>
   </div>
